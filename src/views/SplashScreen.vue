69701ba4886c6ef749311bf21a1dc50514bc051f
Melchiah <melchiahmauck@gmail.com>
2023-03-30 14:17:12 -0700

    Publishing 1.10.4

--- src/views/SplashScreen.vue
+++ src/views/SplashScreen.vue
@@ -3,7 +3,7 @@ import { onMounted, Ref, ref } from 'vue';
 import B, { Sty, Axis, Align,mdColors, Spacing } from './utils/B.vue';
 import { CapacitorUpdater } from '@capgo/capacitor-updater';
 import { SplashScreen } from '@capacitor/splash-screen';
-import Viewport from './components/Page.vue';
+import Page from './components/Page.vue';
 import updatingIcon from '@/assets/download_FILL1_wght400_GRAD0_opsz48.svg';
 import router from '@/router';
 const logoWidth = 4;
@@ -30,39 +30,39 @@ async function getAndApplyPatch() {
     await CapacitorUpdater.set({ id: patchData.id });
   }
 
+  await SplashScreen.hide();
   // Start the app
   //router.push('/home');
 }
 </script>
 
 <template>
-  <Viewport>
-    <B :sty="{
-      width: `1f`,
-      height: `1f`,
-      background: mdColors.green,
-      align: Align.center,
-      spacing: 0.5,
-    }">
-      <div class="prevent-select">
-        <!--<B :sty="{height: textSize}"></B>-->
-        <B :sty="{
-          width: logoWidth,
-          height: logoWidth,
-        }">
-          <img style="width: 100%; height: 100%" :src="updatingIcon" alt="Updating Icon" />
-        </B>
-        <B
-          :sty="{
-            height: textSize,
-            textSize: textSize,
-            textColor: mdColors.white,
-          }"
-        >
-        {{updatingText}}
-        </B>
-        <B :sty="{height: textSize}"></B>
-      </div>
-    </B>
-  </Viewport>
+  <Page>
+    <div class="prevent-select">
+      <B :sty="{
+        width: `1f`,
+        height: `1f`,
+        background: mdColors.green,
+        align: Align.center,
+      }">
+          <!--<B :sty="{height: textSize}"></B>-->
+          <B :sty="{
+            width: logoWidth,
+            height: logoWidth,
+          }">
+            <img style="width: 100%; height: 100%" :src="updatingIcon" alt="Updating Icon" />
+          </B>
+          <B
+            :sty="{
+              height: textSize,
+              textSize: textSize,
+              textColor: mdColors.white,
+            }"
+          >
+          {{updatingText}}
+          </B>
+          <B :sty="{height: textSize}"></B>
+      </B>
+    </div>
+  </Page>
 </template>
