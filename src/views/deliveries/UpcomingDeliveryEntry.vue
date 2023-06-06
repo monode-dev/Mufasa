93324a31436741ab765a4257df11807369c71803
Melchiah <melchiahmauck@gmail.com>
2023-06-06 16:43:29 -0700

    Publishing 1.24.16

--- src/views/deliveries/UpcomingDeliveryEntry.vue
+++ src/views/deliveries/UpcomingDeliveryEntry.vue
@@ -11,12 +11,12 @@ const props = defineProps({
 const amount = ref(0);
 
 // Auto
-const client = ref(`None`);
-const tank = ref(`None`);
+const client = ref(`Client A`);
+const tank = ref(`Tank A`);
 
 // Manual
 const clientName = ref(`Client A`);
-const fuelType = ref(`None`);
+const fuelType = ref(`Fuel Type A`);
 </script>
 
 <template>
@@ -28,52 +28,68 @@ const fuelType = ref(`None`);
         spacing: 1,
       }"
     >
-      <DropDown
-        label="Client"
-        v-model:selected="client"
-        :getKeyFromData="(data: any) => data"
-        :options="[
-          {
-            label: `None`,
-            data: `None`,
-          },
-          {
-            label: `Client A`,
-            data: `Client A`,
-          },
-          {
-            label: `Client B`,
-            data: `Client B`,
-          },
-          {
-            label: `Client C`,
-            data: `Client C`,
-          },
-        ]"
-      />
-      <DropDown
-        label="Tank"
-        v-model:selected="tank"
-        :getKeyFromData="(data: any) => data"
-        :options="[
-          {
-            label: `None`,
-            data: `None`,
-          },
-          {
-            label: `Tank A`,
-            data: `Tank A`,
-          },
-          {
-            label: `Tank B`,
-            data: `Tank B`,
-          },
-          {
-            label: `Tank C`,
-            data: `Tank C`,
-          },
-        ]"
-      />
+      <Row
+        :sty="{
+          width: `1f`,
+          spacing: 1,
+        }"
+      >
+        <DropDown
+          label="Client"
+          v-model:selected="client"
+          :getKeyFromData="(data: any) => data"
+          :options="[
+            {
+              label: `None`,
+              data: `None`,
+            },
+            {
+              label: `Client A`,
+              data: `Client A`,
+            },
+            {
+              label: `Client B`,
+              data: `Client B`,
+            },
+            {
+              label: `Client C`,
+              data: `Client C`,
+            },
+          ]"
+        />
+        <DeleteOptionsButton />
+      </Row>
+      <Row
+        :sty="{
+          width: `1f`,
+          spacing: 1,
+        }"
+      >
+        <DropDown
+          label="Tank"
+          v-model:selected="tank"
+          :getKeyFromData="(data: any) => data"
+          :options="[
+            {
+              label: `None`,
+              data: `None`,
+            },
+            {
+              label: `Tank A`,
+              data: `Tank A`,
+            },
+            {
+              label: `Tank B`,
+              data: `Tank B`,
+            },
+            {
+              label: `Tank C`,
+              data: `Tank C`,
+            },
+          ]"
+        />
+        <Label label="Amount"><Field v-model:value="amount" /></Label>
+      </Row>
     </Box>
     <Box
       v-if="!isAuto"
@@ -82,34 +98,49 @@ const fuelType = ref(`None`);
         spacing: 1,
       }"
     >
-      <Label label="Client Name"
-        ><Field v-model:value="clientName" hint="Client Name"
-      /></Label>
-      <DropDown
-        label="Fuel Type"
-        v-model:selected="fuelType"
-        :getKeyFromData="(data: any) => data"
-        :options="[
-          {
-            label: `None`,
-            data: `None`,
-          },
-          {
-            label: `Fuel Type A`,
-            data: `Fuel Type A`,
-          },
-          {
-            label: `Fuel Type B`,
-            data: `Fuel Type B`,
-          },
-          {
-            label: `Fuel Type C`,
-            data: `Fuel Type C`,
-          },
-        ]"
-      />
+      <Row
+        :sty="{
+          width: `1f`,
+          spacing: 1,
+        }"
+      >
+        <Label label="Client Name"
+          ><Field v-model:value="clientName" hint="Client Name"
+        /></Label>
+        <DeleteOptionsButton />
+      </Row>
+      <Row
+        :sty="{
+          width: `1f`,
+          spacing: 1,
+        }"
+      >
+        <DropDown
+          label="Fuel Type"
+          v-model:selected="fuelType"
+          :getKeyFromData="(data: any) => data"
+          :options="[
+            {
+              label: `None`,
+              data: `None`,
+            },
+            {
+              label: `Fuel Type A`,
+              data: `Fuel Type A`,
+            },
+            {
+              label: `Fuel Type B`,
+              data: `Fuel Type B`,
+            },
+            {
+              label: `Fuel Type C`,
+              data: `Fuel Type C`,
+            },
+          ]"
+        />
+        <Label label="Amount"><Field v-model:value="amount" /></Label>
+      </Row>
     </Box>
-    <Label label="Amount"><Field v-model:value="amount" /></Label>
     <Button outlined :sty="{ width: `1f` }">Complete</Button>
   </Card>
 </template>
