bc2e245ffee548d90158e88be4499373e775ed26
Melchiah <melchiahmauck@gmail.com>
2023-07-03 10:49:42 -0700

    Publishing 1.28.20

--- src/views/components/AutoUpdateLoadingScreen.vue
+++ src/views/components/AutoUpdateLoadingScreen.vue
@@ -26,16 +26,16 @@ let appStartRequested = false;
 })();
 
 // If have not checked for updates after a short bit, then apply the update after start
-setTimeout(() => {
-  if (!haveCheckedForUpdates) {
-    appStartRequested = true;
-  }
-}, 5 * 1000);
-setTimeout(() => {
-  if (!haveDownloadedUpdate) {
-    appStartRequested = true;
-  }
-}, 10 * 1000);
+// setTimeout(() => {
+//   if (!haveCheckedForUpdates) {
+//     appStartRequested = true;
+//   }
+// }, 5 * 1000);
+// setTimeout(() => {
+//   if (!haveDownloadedUpdate) {
+//     appStartRequested = true;
+//   }
+// }, 10 * 1000);
 
 // Check for updates;
 (async () => {
@@ -56,7 +56,7 @@ setTimeout(() => {
       haveDownloadedUpdate = true;
 
       // Apply the update at the right time.
-      if (!appStartRequested) {
+      if (appStartRequested) {
         // Apply the patch on close
         App.addListener("appStateChange", async ({ isActive }) => {
           if (!isActive) {
