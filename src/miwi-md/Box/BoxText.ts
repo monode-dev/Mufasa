a61d6eaeb552716c79673c167685519418eb60ed
Melchiah <melchiahmauck@gmail.com>
2023-07-04 16:06:56 -0700

    Publishing 1.28.32

--- src/miwi-md/Box/BoxText.ts
+++ src/miwi-md/Box/BoxText.ts
@@ -28,7 +28,7 @@ export type TextSty = {
 // const fontSizeToHtmlUnit = 0.9;
 export function numToFontSize(num: number) {
   // return sizeToCss(fontSizeToHtmlUnit * num);
-  return sizeToCss(num * 0.95);
+  return sizeToCss(num); // * 0.95);
 }
 
 export function computeTextStyle(
