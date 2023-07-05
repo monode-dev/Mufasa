5e10a1f9e309ff78313240ab07f823a0be505f51
Melchiah <melchiahmauck@gmail.com>
2023-07-04 18:30:14 -0700

    Publishing 1.28.38

--- src/miwi-md/Box/Box.vue
+++ src/miwi-md/Box/Box.vue
@@ -140,9 +140,9 @@ function updateFromHtml(divRef: HTMLElement | undefined) {
 .b-x::before {
   content: "";
   position: absolute;
-  top: -0.25rem;
-  right: -0.25rem;
-  bottom: -0.25rem;
-  left: -0.25rem;
+  top: -0.375rem;
+  right: -0.375rem;
+  bottom: -0.375rem;
+  left: -0.375rem;
 }
 </style>
