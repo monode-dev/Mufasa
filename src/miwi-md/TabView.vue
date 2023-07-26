6d6f3e87c62d3f69bd1ccc7f78717460b3a948d3
Melchiah <melchiahmauck@gmail.com>
2023-07-26 09:54:28 -0700

    Patched tabview bug.

--- src/miwi-md/TabView.vue
+++ src/miwi-md/TabView.vue
@@ -35,7 +35,7 @@ function selectTab(newTab: number) {
 const tabBodiesParent = ref<ComponentPublicInstance | null>(null);
 watchEffect(() => {
   const newTab = props.selectedTab;
-  const newTabPosition = newTab === 0 ? `100vw` : newTab === 1 ? 0 : `-100vw`;
+  const newTabPosition = newTab === 0 ? 0 : newTab === 1 ? `-100vw` : `-200vw`;
   if (tabBodiesParent.value?.$el) {
     gsap.to(tabBodiesParent.value.$el, {
       duration: 0.15,
