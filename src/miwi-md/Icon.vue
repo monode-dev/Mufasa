940abd459ea138d55d7806473e7a930f95255819
Melchiah Mauck <MelchiahMauck@gmail.com>
2023-08-18 14:10:07 -0400

    Tried to delay pushing field changes to DB, and fixed wierd css flex centering bug.

--- src/miwi-md/Icon.vue
+++ src/miwi-md/Icon.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 // https://pictogrammers.com/library/mdi/
-import { defineProps, PropType } from "vue";
+import { PropType } from "vue";
 import { FlexSize } from "./Box/BoxSize";
 import { mdColors } from "./Box/BoxDecoration";
+import * as mdijs from "@mdi/js";
 
 // Create a prop called size
 const props = defineProps({
@@ -18,24 +19,31 @@ const props = defineProps({
     type: [Number, String, Object] as PropType<number | string | FlexSize>,
     default: 1,
   },
-  onClick: {
-    type: [Function, undefined, null] as PropType<
-      (e: MouseEvent) => void | undefined | null
-    >,
-    default: undefined,
-  },
 });
+
+// From: https://github.com/therufa/mdi-vue/blob/master/src/shared.js
+const ucFirst = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+const toMdiName = (str: string) => str.split("-").map(ucFirst).join("");
 </script>
 
 <template>
   <Box
-    :onClick="onClick"
     :sty="{
       width: scale,
       height: scale,
       textColor: color,
+      overflowY: $Overflow.crop,
     }"
   >
-    <mdicon :name="icon" width="100%" height="100%" />
+    <!-- From: https://github.com/therufa/mdi-vue/blob/master/v3.js -->
+    <svg
+      fill="currentColor"
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path :d="(mdijs as any)[`mdi${toMdiName(icon)}`]"></path>
+    </svg>
   </Box>
 </template>
