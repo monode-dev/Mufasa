664d9f29f74a21223926ed2b9c4d5cd317de9dfe
MechMel <melchiahmauck@gmail.com>
2024-02-15 21:34:28 -0800

    Publishing 0.2.17

--- src/Doc.ts
+++ src/Doc.ts
@@ -85,7 +85,9 @@ function _initializeInst<T extends Doc>(
         ...(isValid(propConfig.toPrim)
           ? {
               set: function (value) {
-                console.log(`Doc.set ${key}: ${value}`);
+                console.log(
+                  `Doc.set ${key}: ${value} - ${propConfig.toPrim!(value)}`,
+                );
                 this._docStore.batchUpdate({
                   [docId]: { [key]: propConfig.toPrim!(value) },
                 });
