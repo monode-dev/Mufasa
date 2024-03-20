a6fac82240818384214c7fbf24293c68a8ad482b
MechMel <melchiahmauck@gmail.com>
2024-03-20 11:23:39 -0700

    Publishing 0.2.121

--- dist/Plugins/SolidJs.js
+++ dist/Plugins/SolidJs.js
@@ -1,11 +1,2 @@
-import { initializeMosa } from "@monode/mosa";
-import { getMosaConfigForSolid } from "@monode/mosa/solid-js";
-import { createEffect, createMemo, createRoot, createSignal, on, onCleanup, } from "solid-js";
-export const solidSessionInterface = initializeMosa(getMosaConfigForSolid({
-    createRoot,
-    createSignal,
-    createMemo,
-    createEffect,
-    on: on,
-    onCleanup,
-}));
+import { mosaForSolid } from "@monode/mosa/solid-js";
+export const solidSessionInterface = mosaForSolid;
--- src/Plugins/SolidJs.ts
+++ src/Plugins/SolidJs.ts
@@ -1,21 +1,3 @@
-import { initializeMosa } from "@monode/mosa";
-import { getMosaConfigForSolid } from "@monode/mosa/solid-js";
-import {
-  createEffect,
-  createMemo,
-  createRoot,
-  createSignal,
-  on,
-  onCleanup,
-} from "solid-js";
+import { mosaForSolid } from "@monode/mosa/solid-js";
 
-export const solidSessionInterface = initializeMosa(
-  getMosaConfigForSolid({
-    createRoot,
-    createSignal,
-    createMemo,
-    createEffect,
-    on: on as any,
-    onCleanup,
-  }),
-);
+export const solidSessionInterface = mosaForSolid;
