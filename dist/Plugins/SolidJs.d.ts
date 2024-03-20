7f218c0cd23e7cccc8c9bff15d0c056d50887d64
MechMel <melchiahmauck@gmail.com>
2024-03-20 11:05:03 -0700

    Publishing 0.2.118

--- dist/Plugins/SolidJs.d.ts
+++ dist/Plugins/SolidJs.d.ts
@@ -1,6 +1,6 @@
 export declare const solidSessionInterface: {
     readonly useRoot: <T>(func: () => T) => T;
-    readonly useProp: <GetType, SetType = GetType>(initValue: GetType | SetType) => import("@monode/mosa").Prop<GetType, SetType>;
+    readonly useProp: <GetType, SetType = GetType>(initValue: GetType) => import("@monode/mosa").Prop<GetType, SetType>;
     readonly useFormula: <GetType_1, Setter extends ((value: any) => any) | undefined = ((value: GetType_1) => any) | undefined>(compute: () => GetType_1, set?: Setter | undefined) => import("@monode/mosa").ReadonlyProp<GetType_1> & (undefined extends Setter ? {} : Setter extends (...args: any[]) => any ? import("@monode/mosa").WriteonlyProp<Parameters<Setter>[0]> : {});
     readonly doNow: <T_1>(func: () => T_1) => T_1;
     readonly doWatch: (func: () => void, options?: Partial<{
