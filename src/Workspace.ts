64f1070f1bbcfba14f88100cd36927af487054fe
MechMel <melchiahmauck@gmail.com>
2024-03-20 17:13:51 -0700

    Publishing 0.2.123

--- src/Workspace.ts
+++ src/Workspace.ts
@@ -1,12 +1,17 @@
 export type WorkspaceIntegration = {
-  onUserMetadata: (handle: (metadata: UserMetadata | null) => void) => () => void;
+  onUserMetadata: (
+    handle: (metadata: UserMetadata | null) => void,
+  ) => () => void;
   createWorkspace: (params: { stage: string }) => Promise<void>;
   createWorkspaceInterface: (params: {
     inviteCode: string;
     workspaceId: string;
     validForDays: number;
   }) => Promise<void>;
-  joinWorkspace: (params: { inviteCode: string; stage: string }) => Promise<void>;
+  joinWorkspace: (params: {
+    inviteCode: string;
+    stage: string;
+  }) => Promise<void>;
   leaveWorkspace: (params: { stage: string } | undefined) => Promise<void>;
   // deleteWorkspace: (params: { stage: string } | undefined) => Promise<void>;
 };
