dd714678e2a79e58685047a0abf8fc9f6c08f026
MechMel <15083231+MechMel@users.noreply.github.com>
2024-05-07 13:52:51 -0700

    Publishing mufasa 0.2.288

--- client/src/DocStore.ts
+++ client/src/DocStore.ts
@@ -368,11 +368,12 @@ export function initializeStoreBank(bankConfig: {
               const newInstSignature = incomingSignature.value;
               const oldInstConfig = currentInstConfig.value;
               console.log(
-                `${params.docType} - newInstSignature: ${JSON.stringify(
-                  newInstSignature,
-                  null,
-                  2,
-                )}`,
+                `newInstSignature:`,
+                // `${params.docType} - newInstSignature: ${JSON.stringify(
+                //   newInstSignature,
+                //   null,
+                //   2,
+                // )}`,
               );
               console.log(
                 `${params.docType} - oldInstConfig: ${JSON.stringify(
