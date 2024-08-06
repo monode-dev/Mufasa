49a75fbe9fbda611e312f31cbf03fcf76d059ac6
MechMel <15083231+MechMel@users.noreply.github.com>
2024-08-06 14:15:13 -0700

    Cleaned up the <ShapeSelector /> component.

--- src/Calculator/ShapeSelector.tsx
+++ src/Calculator/ShapeSelector.tsx
@@ -1,7 +1,5 @@
 import {
   Label,
-  Row,
-  // Selector,
   Prop,
   Txt,
   exists,
@@ -21,66 +19,47 @@ export default function ShapeSelector(
     label: string | undefined;
   }>,
 ) {
-  // const value = useProp(props.value);
   const dropDownIsOpen = useProp(false);
-
-  function selectTankShape(shape: TankShapeId) {
-    console.log("selectTankShape", shape);
-    dropDownIsOpen.value = false;
-    props.value.value = shape;
-  }
-
   return (
-    <Row padBetween={0.75}>
-      <Label label={props.label}>
-        <Selector
-          value={props.value.value}
-          modalIsOpenSig={dropDownIsOpen}
-          noneLabel="Select Shape"
-          getLabelForData={(shapeId: TankShapeId | null) => {
-            if (exists(shapeId)) {
-              const tankShape = getTankShape(shapeId);
-              return tankShape.nameLong;
-            }
-            return null;
-          }}
-        >
-          <For each={TANK_SHAPE_IDS} fallback={<Txt>Loading...</Txt>}>
-            {(shapeId) => (
-              <Txt
-                onClick={() => {
-                  selectTankShape(shapeId);
-                  dropDownIsOpen.value = false;
-                }}
-                widthGrows
-                heightShrinks
-                overflowX={$Overflow.wrap}
-              >
-                {getTankShape(shapeId).nameLong}
-              </Txt>
-            )}
-          </For>
-        </Selector>
-          <Icon 
-            iconPath={mdiHelpCircleOutline} 
-            onClick={()=> {
-              pushPage(InfoCard, {entriesToOpen: ["Tank","Shape", getTankShape(props.value.value)?.nameLong ?? ""]});
-            }}
-          >
-        </Icon>
-      </Label>
-      {/* <Show when={exists(props.value.value)}>
-        <Icon
-          iconPath={mdiHelp}
-          stroke={mdColors.grey}
-          scale={0.875}
-          onClick={() => {
-            pushPage(ShapeHelpDialog, {
-              shape: props.value,
-            });
-          }}
-        />
-      </Show> */}
-    </Row>
+    <Label label={props.label} padBetween={0.75}>
+      <Selector
+        value={props.value.value}
+        modalIsOpenSig={dropDownIsOpen}
+        noneLabel="Select Shape"
+        getLabelForData={(shapeId: TankShapeId | null) => {
+          if (!exists(shapeId)) return null;
+          const tankShape = getTankShape(shapeId);
+          return tankShape.nameLong;
+        }}
+      >
+        <For each={TANK_SHAPE_IDS} fallback={<Txt>Loading...</Txt>}>
+          {(shapeId) => (
+            <Txt
+              onClick={() => {
+                props.value.value = shapeId;
+                dropDownIsOpen.value = false;
+              }}
+              widthGrows
+              heightShrinks
+              overflowX={$Overflow.wrap}
+            >
+              {getTankShape(shapeId).nameLong}
+            </Txt>
+          )}
+        </For>
+      </Selector>
+      <Icon
+        iconPath={mdiHelpCircleOutline}
+        onClick={() => {
+          pushPage(InfoCard, {
+            entriesToOpen: [
+              "Tank",
+              "Shape",
+              getTankShape(props.value.value)?.nameLong ?? "",
+            ],
+          });
+        }}
+      />
+    </Label>
   );
 }
