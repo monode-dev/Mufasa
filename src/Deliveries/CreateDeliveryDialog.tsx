1d5a7e381e52cc3cfacaf766e0406fb25b1c9c2f
fileman42 <fileman.mauck@gmail.com>
2024-08-13 08:30:28 -0700

    One time client for delivery works now.

--- src/Deliveries/CreateDeliveryDialog.tsx
+++ src/Deliveries/CreateDeliveryDialog.tsx
@@ -65,10 +65,10 @@ function CreateDeliveryDialog(props: {
     set phoneNumber(v) {
       phoneNumber.value = v;
     },
-    get explicitAddress() {
+    get address() {
       return explicitAddress.value;
     },
-    set explicitAddress(v) {
+    set address(v) {
       explicitAddress.value = v;
     },
     get notes() {
@@ -96,7 +96,7 @@ function CreateDeliveryDialog(props: {
           : selectedClient.value,
       _manualTitle: title.value,
       _manualPhoneNumber: phoneNumber.value,
-      address: explicitAddress.value,
+      _manualClientAddress: explicitAddress.value,
       notes: notes.value,
       sortPosition: Date.now(),
       creationTimePosix: Date.now(),
