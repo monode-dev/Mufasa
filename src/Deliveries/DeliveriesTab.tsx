6012db516595c033f3eaa54b0321e9f76a85d86e
fileman42 <fileman.mauck@gmail.com>
2024-08-29 16:46:57 -0700

    Updated scheduled delivery notifications to a card

--- src/Deliveries/DeliveriesTab.tsx
+++ src/Deliveries/DeliveriesTab.tsx
@@ -21,7 +21,6 @@ import { DailyTotalsCard } from "./DailyTotalsCard";
 import { Client } from "@/Clients/Client";
 
 export default function DeliveriesTab() {
-  //const isChecked = useProp(false);
   return (
     <Body padBetween={0.5}>
       {/* SECTION: Daily Totals */}
@@ -48,32 +47,26 @@ export default function DeliveriesTab() {
         </Box>
       </Row>
       <Column padBetween={1}>
-      <For each={[...(Client.getAllDocs() ?? [])]}>
-        {(client) => (
-          <Show when={Date.now() >= client.scheduledDeliveryStartDate! && client.scheduledDeliveryStartDate && client.shouldScheduleDeliveriesForThisClient}>
-            <Row padBetween={0.3} padRight={0.6}>
-              <Icon iconPath={mdiCircleSmall} scale={2} stroke={$theme.colors.warning} />
-              <Txt stroke={$theme.colors.warning} bold widthGrows>{client.name} is due for a delivery.</Txt>
-              <Box
-                bonusTouchArea
-                width={1}
-                height={1}
-                outlineSize={1 / 8}
-                cornerRadius={1 / 7}
-                onClick={() => {
-                  //isChecked.value = !isChecked.value;
-                  client.scheduledDeliveryStartDate = client.scheduledDeliveryStartDate! + (client.weeksBetweenScheduledDeliveries! * 604800000);
-                }}
-                outlineColor={$theme.colors.text}
-              >
-                {/* <Show when={isChecked.value}>
-                  <Icon iconPath={mdiCheck} scale={0.8} stroke={$theme.colors.text} />
-                </Show> */}
-              </Box>
-            </Row>
-          </Show>
-        )}
-      </For>
+      <Show when={[...(Client.getAllDocs() ?? [])].filter((client) => client.shouldScheduleDeliveriesForThisClient && client.scheduledDeliveryStartDate && Date.now() >= client.scheduledDeliveryStartDate).length >= 1}>
+        <Card>
+          <Txt h2 alignTopLeft widthGrows singleLine>Notifications:</Txt>
+          <For each={[...(Client.getAllDocs() ?? [])]}>
+            {(client) => (
+              <Show when={client.shouldScheduleDeliveriesForThisClient && client.scheduledDeliveryStartDate && Date.now() >= client.scheduledDeliveryStartDate}>
+                <Row padBetween={0.3} padRight={0.6}>
+                  <Icon iconPath={mdiCircleSmall} scale={2} />
+                  <Txt bold widthGrows>{client.name} is due for a delivery.</Txt>
+                  <Icon iconPath={mdiCheck} scale={1.4} stroke={$theme.colors.primary}                 
+                    onClick={() => {
+                      client.scheduledDeliveryStartDate = client.scheduledDeliveryStartDate! + (client.weeksBetweenScheduledDeliveries! * 604800000);
+                    }} 
+                  />
+                </Row>
+              </Show>
+            )}
+          </For>
+        </Card>
+      </Show>
         <SortableColumn
           onSort={(props) =>
             FloatSort.moveItem({
