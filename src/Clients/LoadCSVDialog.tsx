1ec1e8a8bff233a0a7cf093c4eb965ba7e6a60a8
MechMel <15083231+MechMel@users.noreply.github.com>
2024-09-05 20:41:04 -0700

    Tried to fix Dialogs.

--- src/Clients/LoadCSVDialog.tsx
+++ src/Clients/LoadCSVDialog.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, mdColors, Page, popPage, Row, Txt } from "miwi";
+import { Button, Card, Dialog, mdColors, Page, popPage, Row, Txt } from "miwi";
 import { Client } from "./Client";
 
 export function LoadCSVDialog() {
@@ -6,21 +6,21 @@ export function LoadCSVDialog() {
     if (csvFile.files === null) {
       return;
     }
-  
+
     let final_vals: string[][] = [];
 
-    let csvReader = new FileReader(); 
-    if(csvFile.files) {
+    let csvReader = new FileReader();
+    if (csvFile.files) {
       const input = csvFile.files[0];
-  
-      csvReader.onload = function(evt) {
-        if (evt.target) {
-          const text = evt.target.result; 
 
-          if(typeof text === 'string' || text instanceof String) {
-            const values = text.split(/[\n]+/); 
-            values.forEach(val => {
-              final_vals.push(val.split(',')); 
+      csvReader.onload = function (evt) {
+        if (evt.target) {
+          const text = evt.target.result;
+
+          if (typeof text === "string" || text instanceof String) {
+            const values = text.split(/[\n]+/);
+            values.forEach((val) => {
+              final_vals.push(val.split(","));
             });
             for (let i = 0; i < final_vals.length; i++) {
               Client.create({
@@ -28,37 +28,37 @@ export function LoadCSVDialog() {
                 clientId: final_vals[i][1],
                 phoneNumber: final_vals[i][2],
                 address: final_vals[i][3],
-                notes: final_vals[i][4]
-              })
+                notes: final_vals[i][4],
+              });
             }
-          }  
+          }
         }
-      }
+      };
       csvReader.readAsText(input as Blob);
     }
     popPage();
   }
 
-  return(
-    <Page onClick={popPage} fill="#00000099">
-      <Card preventClickPropagation width={`75%`} shadowSize={0}>
-        <Txt>Select a CSV file</Txt>
-        <input class="form-control" id="csvFile" type="file" accept=".csv"/>
-        <Row widthGrows align={$Align.spaceEvenly}>
-          <Button outlined onClick={popPage}>
-            Cancel
-          </Button>
-          <Button 
-          onClick={() =>{
-            const csvFile = document.getElementById('csvFile') as HTMLInputElement;
+  return (
+    <Dialog>
+      <Txt>Select a CSV file</Txt>
+      <input class="form-control" id="csvFile" type="file" accept=".csv" />
+      <Row widthGrows align={$Align.spaceEvenly}>
+        <Button outlined onClick={popPage}>
+          Cancel
+        </Button>
+        <Button
+          onClick={() => {
+            const csvFile = document.getElementById(
+              "csvFile",
+            ) as HTMLInputElement;
             handleYes(csvFile);
-          }} 
+          }}
           fill={mdColors.green}
-          >
-            Create
-          </Button>
-        </Row>
-      </Card>
-    </Page>
+        >
+          Create
+        </Button>
+      </Row>
+    </Dialog>
   );
 }
